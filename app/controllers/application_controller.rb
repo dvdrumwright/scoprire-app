0c5807215613cb09600e84a58d8dba50c4116e39
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-12-23 16:12:21 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :sessions_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -10,5 +12,16 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= Cyclist.find(session[:user_id]) if session[:user_id]
+    end
+  end
+
 
 end
