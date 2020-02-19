e34db86203b6758fed08eda1ea3444258972017b
Daniel  Drumwright <danieldrumwright@gmail.com>
2020-02-19 03:57:59 +0000

    update

--- app/controllers/cyclists_controller.rb
+++ app/controllers/cyclists_controller.rb
@@ -4,26 +4,25 @@ require 'sinatra'
 class CyclistsController < ApplicationController
 
    get '/signup' do
-      if !logged_in?
-        erb :'/cyclists/new', locals: {message: "Please sign up before you sign in"}
+      if logged_in?
+         redirect to "/cyclists/#{current_user.id}"
       else
-        redirect to '/index'
+        erb :'/cyclists/new'
       end
     end
-  
+
     post '/signup' do
        if params[:username] == "" || params[:email] == "" || params[:password] == ""
           flash[:sign_up] = "Please make sure to fill out all the fields"
           redirect to '/signup'
         end
-        if @a = !Cyclist.new(:username => params[:username], :password => params[:password])
-            @a.valid?
-            flash[:sign_up] = "Sorry name is already taken !"
-            redirect to '/signup'
-        elsif Cyclist.new(:email => params[:email], :password => params[:password]).valid?
+
+        if !Cyclist.new(:username => params[:username], :password => params[:password])
+           redirect to '/signup'
+        elsif !Cyclist.new(:email => params[:email], :password => params[:password])
             flash[:sign_up] = "Sorry email address is already taken!"
             redirect to '/signup'
-          else
+        else
             @user = Cyclist.new(:username => params[:username], :email => params[:email],  :password => params[:password])
             @user.save
             session[:user_id] = @user.id
@@ -32,18 +31,19 @@ class CyclistsController < ApplicationController
       end
 
       get '/login' do
-         if !logged_in?
-           erb :'cyclists/login'
+         if logged_in?
+           redirect to "/cyclists/#{current_user.id}"
          else
-          redirect to '/rides'
+          erb :'/cyclists/login'
         end
       end
 
     post '/login' do
-      user = Cyclist.find_by(:username => params[:username])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect '/rides'
+      @user = Cyclist.find_by(:username => params[:username], :pasword => params[:password])
+
+      if !@user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect to '/rides'
       else
         flash[:message] = "Your Username or Password is Incorrect."
         erb :'cyclists/login'
