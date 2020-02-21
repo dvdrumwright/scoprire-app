89c6351159b50c82f68d89769a7f47e06143d3c5
Daniel  Drumwright <danieldrumwright@gmail.com>
2020-02-21 00:39:23 +0000

    Done.

--- app/controllers/cyclists_controller.rb
+++ app/controllers/cyclists_controller.rb
@@ -3,59 +3,51 @@ require 'sinatra'
 
 class CyclistsController < ApplicationController
 
-   get '/signup' do
-      if logged_in?
-         redirect to "/cyclists/#{current_user.id}"
-      else
-        erb :'/cyclists/new'
-      end
-    end
-
-    post '/signup' do
-       if params[:username] == "" || params[:email] == "" || params[:password] == ""
-          flash[:sign_up] = "Please make sure to fill out all the fields"
-          redirect to '/signup'
-        end
-
-        if !Cyclist.new(:username => params[:username], :password => params[:password])
-           redirect to '/signup'
-        elsif !Cyclist.new(:email => params[:email], :password => params[:password])
-            flash[:sign_up] = "Sorry email address is already taken!"
-            redirect to '/signup'
+  get '/signup' do
+        if !logged_in?
+           erb :'/cyclists/new'
         else
-            @user = Cyclist.new(:username => params[:username], :email => params[:email],  :password => params[:password])
-            @user.save
-            session[:user_id] = @user.id
-           redirect to '/login'
+          redirect to '/rides'
         end
       end
 
       get '/login' do
          if logged_in?
-           redirect to "/cyclists/#{current_user.id}"
+           redirect to '/rides'
          else
           erb :'/cyclists/login'
         end
       end
 
-    post '/login' do
-      @user = Cyclist.find_by(:username => params[:username], :pasword => params[:password])
+      post '/login' do
+        @user = Cyclist.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/rides'
+        else
+            redirect '/signup'
+        end
+      end
 
-      if !@user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect to '/rides'
-      else
-        flash[:message] = "Your Username or Password is Incorrect."
-        erb :'cyclists/login'
+      post '/signup' do
+         if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+         else
+              @user = Cyclist.create(:username => params[:username], :email => params[:email],  :password => params[:password])
+              @user.save
+              session[:user_id] = @user.id
+             redirect to '/rides'
+          end
+        end
+
+
+
+      get '/logout' do
+        if logged_in?
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
       end
     end
-
-    get '/logout' do
-      if logged_in?
-        session.destroy
-        redirect to '/login'
-      else
-        redirect to '/'
-      end
-    end
-  end
