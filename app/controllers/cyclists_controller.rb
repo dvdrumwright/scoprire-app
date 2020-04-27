b2fc04e00d41b78eacee0f559c0ae4a06776bea2
Daniel  Drumwright <danieldrumwright@gmail.com>
2020-04-27 00:03:41 +0000

    Done.

--- app/controllers/cyclists_controller.rb
+++ app/controllers/cyclists_controller.rb
@@ -13,47 +13,51 @@ class CyclistsController < ApplicationController
     if !logged_in?
         erb :'/cyclists/new'
     else
-        redirect '/rides'
-    end
+        redirect '/cyclists/login'
+     end
    end
 
-   get '/login' do
-     if logged_in?
-        redirect '/rides'
-      else
-        erb :'/cyclists/login'
-      end
-    end
-
-   post '/login' do
-    user = Cyclist.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-       session[:user_id] = user.id
-       redirect  "/rides"
-     else
-       redirect "/signup"
-    end
-  end
-
-   post '/signup' do
+   post '/cyclists/new' do
      if params[:username] == "" || params[:password] == "" || params[:email] == ""
-        redirect '/signup'
-     else
+        redirect '/cyclists/new'
+      else
         @user = Cyclist.create(username: params[:username], password: params[:password], email: params[:email])
         @user.save
         session[:user_id] = @user.id
         erb :'/rides/new'
-        # redirect '/rides'
         end
       end
 
-    get '/logout' do
-      if logged_in?
-            session.destroy
-            redirect to '/login'
-        else
-          redirect '/'
-       end
-     end
+  get '/logout' do
+    if logged_in?
+        session.destroy
+        redirect to '/login'
+    else
+        redirect to '/'
+      end
+    end
 
-   end
+  get '/login' do
+    if !logged_in?
+       erb :'/cyclists/login'
+    else
+      redirect "rides/new"
+      end
+  end
+
+ post '/cyclists/login' do
+      user = Cyclist.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/rides"
+    else
+        flash[:error] = "Incorrect username or password. Please try again!"
+        redirect "/login"
+      end
+    end
+
+
+
+
+
+end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -5,73 +5,24 @@ get '/rides' do
    if logged_in?
      erb :'/rides/new'
    else
-     redirect to '/'
-   end
- end
-
- post '/rides' do
-   if logged_in?
-    @cyclists = Cyclist.find_by_id[:user_id]
-    @rides = @cyclists.rides.create(:location => params[:location], :discription => params[:discription], :ride_distance => params[:ride_distance],:ride_date => params[:ride_date])
-    @rides = current_user
-    if @rides.save
-       redirect to "/rides/#{@rides.id}"
-   else
-     redirect to '/rides/new'
-   end
-    else
-      redirect to '/'
-   end
- end
-
- get '/rides/:id' do
-   if logged_in?
-    @ride = Ride.find_by_id(params[:ride_id])
-  else
-    @rides = Cyclist.new
-  end
-   erb :'/rides/show'
- end
-
-
- get '/rides/:id/edit' do
-   if logged_in?
-     @rides  = Ride.find_by_id(params[:ride_id])
-   if logged_in? && current_user.rides.include?(@rides)
-      erb :'/rides/edit'
-   else
-     redirect to "/rides/#{ @rides.ride_id}"
-   end
- else
-   redirect to '/login'
- end
-end
-#
- patch '/rides/:id' do
-   if @rides = Cyclist.find_by_id(params[:id])
-
-    @rides.cyclist = Cyclist.find_or_create_by(username: params[:cyclist][:username], user_id: current_user.id)
-    @rides.location = params[:location]
-    @rides.ride_date = Date.parse(params[:ride_date])
-    @rides.discription = params[:discription]
-    @rides.title = params[:title]
- elsif
-     logged_in? && current_user.cyclists.include?(@rides)
-     @ride.save
-     redirect to "/cyclists/#{@rides.id}"
-   else
-     redirect to "/rides/#{ @rides.id}"
-   end
- end
-end
-
- delete '/rides/:id/delete' do
-   if logged_in?
-        @rides = Ride.find_by_id(params[:ride_id])
-        @rides && @rides.cyclists == current_user
-        @rides.delete
-      redirect to '/rides'
-   else
      redirect to '/login'
-     end
    end
+ end
+
+  post '/rides/new' do
+    if params.empty?
+      redirect '/rides/new'
+    elsif logged_in? && !params.empty?
+      @current_ride = current_user.rides.create(title: params[:title], location: params[:location], description: params[:description], ride_distance: params[:ride_distance], ride_date: params[:ride_date])
+      if @current_ride.save
+          redirect "/rides/#{@current_ride.id}"
+        else redirect '/rides/new'
+          end
+          else
+          flash[:error] = "You must be logged in to see the projects index."
+           redirect '/login'
+         end
+           current_user.save
+        end
+    
+    end
