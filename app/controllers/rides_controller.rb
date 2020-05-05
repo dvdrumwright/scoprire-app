54017f591cac95f169bde727ca8bbb06671522fc
Daniel  Drumwright <danieldrumwright@gmail.com>
2020-05-05 01:45:23 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,86 +1,60 @@
 class RidesController < ApplicationController
 
-
   get '/rides' do
-          if logged_in?
-              @rides = Ride.all
-              erb :'/rides/show'
-          else
-              redirect '/login'
-          end
+    erb :'/rides/new'
+ end
+
+  post '/rides/new' do
+     if params[:title] == "" || params[:location] == "" || params[:description] == "" || params[:ride_distance] == "" || params[:ride_date] == ""
+        flash[:error] = "All fields must be filled in"
+        redirect '/rides/new'
       end
 
-      get '/rides/new' do
-          if logged_in?
-              erb :'/rides/new'
-          else
-              redirect '/login'
-          end
-      end
-
-      post '/rides' do
-          if params.empty?
-              flash[:error] = "All fields must be filled in"
-              redirect '/rides/new'
-          elsif logged_in? && !params.empty?
-                 @rides= current_user.projects.create(title: params[:title], location: params[:location], description: params[:description], ride_distance: params[:ride_distance], ride_date: params[:ride_date])
-              if @rides.save
-                  redirect "/rides/#{@rides.id}"
-              else
-                  flash[:error] = "Your project could not be saved. Try again!"
-                  redirect '/rides/new'
-              end
-          else
-              flash[:error] = "You must be logged in to see the projects index."
-              redirect '/login'
-          end
-          current_user.save
-      end
-
-      get '/rides/:id' do
-          if logged_in?
-              @rides = Ride.find_by_id(params[:id])
-              erb :'/rides/show'
-          else
-              flash[:error] = "You must be logged in to view projects."
-              redirect '/login'
-          end
-      end
-
-      get '/rides/:id/edit' do
-          @rides = Ride.find_by_id(params[:id])
-          if logged_in? && current_user.projects.include?(@rides)
-              erb :'/rides/edit'
-          else
-              flash[:error] = "You must be logged in to edit a project."
-              redirect '/login'
-          end
-      end
-
-      patch '/rides/:id' do
-        @rides = Ride.find_by_id(params[:id])
-          if params.empty?
-              flash[:error] = "All fields must be filled in"
-              redirect "/rides/#{@rides.id}/edit"
-          elsif logged_in? && !params.empty? && current_user.projects.include?(@rides)
-            @rides.update(title: params[:title], location: params[:location], description: params[:description], ride_distance: params[:ride_distance], ride_date: params[:ride_date])
-              redirect "/rides/#{@rides.id}"
-          else
-              flash[:error] = "You must be logged in."
-              redirect '/login'
-          end
-
-      end
-
-
-    delete '/rides/:id/delete' do
-          if logged_in?
-              @ride = Ride.find_by_id(params[:id])
-              if @ride.user == current_user then @ride.delete else redirect '/login' end
-          else
-              flash[:error] = "You must be logged in."
-              redirect '/login'
-          end
-          redirect '/rides'
+  if !Ride.new(:title => params[:title], :location => params[:location], :description => params[:description], :ride_distance => params[:ride_distance], :ride_date=> params[:ride_date]).valid?
+      flash[:signup_page_message] ="Oops, sorry but that name is already taken!"
+      redirect to '/rides/new'
+  elsif @ride = Ride.new(:title => params[:title], :location => params[:location], :description => params[:description], :ride_distance => params[:ride_distance], :ride_date=> params[:ride_date])
+        @ride.save
+      session[:user_id] = @ride.id
+      redirect to '/rides'
       end
   end
+
+  get '/rides/:id' do
+    if logged_in?
+      @rides = Ride.find_by_id(params[:id])
+     erb :'rides/show'
+   else
+     redirect to '/login'
+   end
+ end
+
+  get '/rides/:id/edit' do
+       @rides = Ride.find_by_id(params[:id])
+  if logged_in? && current_user.rides.include?(@rides)
+     erb :'/rides/edit'
+  else
+    redirect '/rides/new'
+    end
+  end
+
+  patch '/rides/:id' do
+      @rides = current_user.rides.find_by_id(params[:id])
+  if params[:title] == "" || params[:location] == "" || params[:description] == "" || params[:ride_distance] == "" || params[:ride_date] == ""
+     @rides.update(title: params[:title], location: params[:location], description: params[:description], ride_distance: params[:ride_distance], ride_date: params[:ride_date])
+     redirect "/rides/#{@rides.id}"
+  else
+     redirect "/rides/#{@rides.id}/edit"
+     end
+  end
+
+  delete '/rides/:id/delete' do
+      @rides = Ride.find_by_id(params[:id])
+  if current_user.rides.include?(@rides)
+    @rides.delete
+     redirect '/rides'
+   else
+      redirect '/rides'
+    end
+  end
+end
